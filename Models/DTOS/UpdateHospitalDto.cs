0dbbe363fb2f0fa1ccbe5855ad38d68c3673a31f
Dabner Orozco <dabner.orozco@ucb.edu.bo>
2025-10-27 13:43:36 -0400

    feat: Creating Endpoint for hospital

---
+++ Models/DTOS/UpdateHospitalDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Security.Models.DTOS
+{
+    public record UpdateHospitalDto
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Type { get; set; }
+    }
+}

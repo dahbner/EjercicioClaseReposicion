667933e18b0063482422717e3c4c5480de86a36a
Dabner Orozco <dabner.orozco@ucb.edu.bo>
2025-10-27 13:52:47 -0400

    feat: Updating Service Interface methods for hospital

--- Services/IHospitalService.cs
+++ Services/IHospitalService.cs
@@ -8,5 +8,8 @@ namespace Security.Services
         Task<IEnumerable<Hospital>> GetAll();
         Task<Hospital> GetOne(Guid id);
         Task<Hospital> CreateHospital(CreateHospitalDto dto);
+        Task<bool> UpdateHospital(Guid id, UpdateHospitalDto dto);
+        Task<bool> DeleteHospital(Guid id);
+        Task<IEnumerable<Hospital>> GetPublicHospitals();
     }
 }
